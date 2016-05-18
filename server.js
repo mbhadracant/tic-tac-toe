38b768f2eddf526851298c1412350eaf352467d6
Mayur Bhadracant <mbhadracant@abcam.com>
2016-05-18 13:10:02 +0100

    Cleaned up code

--- server.js
+++ server.js
@@ -8,38 +8,82 @@ var clients = [];
 var player1 = null;
 var player2 = null;
 
+var gameStates = ['waiting', 'ready', 'game', 'completed']
 
-app.get("/", function(req, res){
-  res.sendFile(__dirname + '/views/index.html');
-  app.use(express.static(__dirname + '/public'));
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+    app.use(express.static(__dirname + '/public'));
+});
+
+io.on('connection', function (socket) {
+
+    events.onConnect(socket);
+
+    socket.on('lock player 1', function () {
+        events.onLockPlayer1(socket);
+    });
+
+    socket.on('lock player 2', function () {
+        events.onLockPlayer2(socket);
+    });
+
+    socket.on('disconnect', function () {
+        events.onDisconnect(socket);
+    });
 
 });
 
-io.on('connection', function(socket) {
-    
-    clients[socket.client.id];
-    console.log("User connected: " + socket.client.id);
+http.listen(3000, function () {
+    console.log("listening on *: 3000");
+});
 
-    socket.on('lock player 1', function(id){
-      player1 = socket;
-      io.emit('lock button player 1');
+
+
+var events = {
+
+    onConnect: function (socket) {
+
+        clients[socket.client.id] = socket;
+        console.log("User connected: " + socket.client.id);
+
+    },
+
+    onLockPlayer1: function (socket) {
+
+        if (helper.checkBothPlayersAreSame(socket.client.id)) {
+            return;
+        }
+
+        player1 = socket;
+        io.emit('lock button player 1');
         socket.emit('change player 1 text');
-      console.log("Player 1 Ready: " + socket.client.id);
-    });
+        console.log("Player 1 Ready: " + socket.client.id);
 
-    socket.on('lock player 2', function(id){
-      player2 = socket;
-      io.emit('lock button player 2');
-      socket.emit('change player 2 text');
-      console.log("Player 2 Ready: " + socket.client.id);
-    });
+    },
 
-  socket.on('disconnect', function(){
-      delete clients[socket.client.id];
-  });
+    onLockPlayer2: function (socket) {
 
-});
+        if (helper.checkBothPlayersAreSame(socket.client.id)) {
+            return;
+        }
 
-http.listen(3000, function(){
-  console.log("listening on *: 3000");
-});
+        player2 = socket;
+        io.emit('lock button player 2');
+        socket.emit('change player 2 text');
+        console.log("Player 2 Ready: " + socket.client.id);
+
+    },
+
+    onDisconnect: function (socket) {
+        delete clients[socket.client.id];
+    }
+
+};
+
+var helper = {
+    
+    checkBothPlayersAreSame: function (id) {
+       return player1 == clients[id] || player2 == clients[id] ? true : false;
+    }
+
+}
