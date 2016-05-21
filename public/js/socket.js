c364133f2df652033ddd72937f99aa5b0ee9f5d0
mbhadracant <MB@MacBook-Pro.local>
2016-05-21 15:10:48 +0100

    game functionality is working - still need to optimise code and remove duplicated code

--- public/js/socket.js
+++ public/js/socket.js
@@ -12,102 +12,115 @@ socket.on('reset button player 2', function() {
     $('#player2-bt').prop('disabled', false);
 });
 
-socket.on('show when waiting', function (data) {
+socket.on('show when waiting', function(data) {
 
     $('#title').html("WAITING FOR TWO PLAYERS TO JOIN...");
     $('#main-menu').show();
-  if(data.player1Button) {
+    if (data.player1Button) {
+        $('#player1-bt').html("PLAYER 1 JOINED");
+        $('#player1-bt').css("background-color", "red");
+        $('#player1-bt').prop('disabled', true);
+    }
+
+    if (data.player2Button) {
+        $('#player2-bt').html("PLAYER 2 JOINED");
+        $('#player2-bt').css("background-color", "red");
+        $('#player2-bt').prop('disabled', true);
+    }
+});
+
+
+socket.on('lock button player 1', function() {
     $('#player1-bt').html("PLAYER 1 JOINED");
     $('#player1-bt').css("background-color", "red");
     $('#player1-bt').prop('disabled', true);
-  }
-
-  if(data.player2Button) {
-    $('#player2-bt').html("PLAYER 2 JOINED");
-    $('#player2-bt').css("background-color", "red");
-    $('#player2-bt').prop('disabled', true);
-  }
 });
 
-
-socket.on('lock button player 1', function () {
-    $('#player1-bt').html("PLAYER 1 JOINED");
-    $('#player1-bt').css("background-color", "red");
-    $('#player1-bt').prop('disabled', true);
-});
-
-socket.on('lock button player 2', function () {
+socket.on('lock button player 2', function() {
     $('#player2-bt').html("PLAYER 2 JOINED");
     $('#player2-bt').css("background-color", "red");
     $('#player2-bt').prop('disabled', true);
 });
 
-socket.on('change player 1 text', function () {
+socket.on('change player 1 text', function() {
     $('#status').html("PLAYER 1");
 });
 
-socket.on('change player 2 text', function () {
+socket.on('change player 2 text', function() {
     $('#status').html("PLAYER 2");
 });
 
-socket.on('game ready', function (){
-  $('#player1-bt').fadeOut();
-  $('#player2-bt').fadeOut();
+socket.on('game ready', function() {
+    $('#main-menu').fadeOut();
+
 
 });
 
-socket.on('change title', function(data){
-  $('#title').html(data.message);
+socket.on('change title', function(data) {
+    $('#title').html(data.message);
 });
 
-socket.on('apply tick', function(data){
-  $('#title').html("PLAYER " + data.turn + " TURN");
-  if(data.turn == 1) { 
-		$( "div[data-position-x='" + data.x + "'][data-position-y='" + data.y + "']").append($( "<div class='cross'><div class='line1'></div><div class='line2'></div></div>" ));
-	
+socket.on('apply tick', function(data) {
+    if (data.turn == 2) {
+        $("div[data-position-x='" + data.x + "'][data-position-y='" + data.y + "']").append($("<div class='cross'><div class='line1'></div><div class='line2'></div></div>"));
 
-	} else { 
-	 $( "div[data-position-x='" + data.x + "'][data-position-y='" + data.y + "']").append($( "<div class='circle'></div>" ));
-	}
-	
+
+    } else {
+        $("div[data-position-x='" + data.x + "'][data-position-y='" + data.y + "']").append($("<div class='circle'></div>"));
+    }
+
 
 });
 
 
-socket.on('show game', function (game){
+socket.on('show game', function(game) {
     $('#title').html("PLAYER " + game.turn + " TURN");
 
-	for(i = 0; i < 3; i++) { 
-	for(j = 0; j < 3; j++) { 
-  	  if(game.board[i][j] != undefined) { 
-		 if(game.board[i][j] == 1) {
-		 	$( "div[data-position-x='" + i + "'][data-position-y='" + j + "']").append($( "<div class='circle'></div>" ));
-		 } else { 
-			$( "div[data-position-x='" + i + "'][data-position-y='" + j + "']").append($( "<div class='cross'><div class='line1'></div><div class='line2'></div></div>" ));
-		 }
-	}
-  }
-}
-    
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            if (game.board[i][j] != undefined) {
+                if (game.board[i][j] == 1) {
+                    $("div[data-position-x='" + i + "'][data-position-y='" + j + "']").append($("<div class='circle'></div>"));
+                } else {
+                    $("div[data-position-x='" + i + "'][data-position-y='" + j + "']").append($("<div class='cross'><div class='line1'></div><div class='line2'></div></div>"));
+                }
+            }
+        }
+    }
+
     $('#game-board').fadeIn();
-   
+
 });
 
-socket.on('show winning positions', function(data){
-  		
-console.log("kek");
-		
-		for(i = 0; i < 3; i++) { 
-			if(data.player == 1) {
-				$( "div[data-position-x='" + data.winningPositions[i].x + "'][data-position-y='" + data.winningPositions[i].y + "']").find("div").css("border-color","green");
-			} else { 
-				if($( "div[data-position-x='" + data.winningPositions[i].x + "'][data-position-y='" + data.winningPositions[i].y + "']").find("div") != undefined) {
-					var cross = $( "div[data-position-x='" + data.winningPositions[i].x + "'][data-position-y='" + data.winningPositions[i].y + "']").find("div")[0];
-					$(cross).children().css("background","green");
-				}
-			}
-		}
+socket.on('show winning positions', function(data) {
+    for (i = 0; i < 3; i++) {
+        if (data.player == 1) {
+            $("div[data-position-x='" + data.winningPositions[i].x + "'][data-position-y='" + data.winningPositions[i].y + "']").find("div").css("border-color", "green");
+        } else {
+            $("div[style*='opacity:0.4']").remove();
+            if ($("div[data-position-x='" + data.winningPositions[i].x + "'][data-position-y='" + data.winningPositions[i].y + "']").find("div") != undefined) {
+                var cross = $("div[data-position-x='" + data.winningPositions[i].x + "'][data-position-y='" + data.winningPositions[i].y + "']").find("div")[0];
+                $(cross).children().css("background", "green");
+            }
+        }
+    }
+});
 
-		
+socket.on('reset', function() {
+
+    $('body').fadeOut(1500, function() {
+        $('#title').html("WAITING FOR TWO PLAYERS TO JOIN...");
+        $('#main-menu').show();
+        $('#game-board').hide();
+        $('.tick-box').empty();
+        $('#player1-bt').html("PLAYER 1 JOIN");
+        $('#player1-bt').css("background-color", "#4CAF50");
+        $('#player1-bt').prop('disabled', false);
+        $('#player2-bt').html("PLAYER 2 JOIN");
+        $('#player2-bt').css("background-color", "#4CAF50");
+        $('#player2-bt').prop('disabled', false);
+        $('#status').html("SPECTATOR");
+        $('body').fadeIn(1500);
+    });
 
 });
--- server.js
+++ server.js
@@ -16,39 +16,39 @@ var winningPositions = null;
 
 var timer = 5;
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
     app.use(express.static(__dirname + '/public'));
 });
 
-io.on('connection', function (socket) {
+io.on('connection', function(socket) {
 
     events.onConnect(socket);
 
     socket.on('lock player 1', function() {
-      events.onLockPlayer1(socket);
+        events.onLockPlayer1(socket);
     });
 
     socket.on('lock player 2', function() {
-      events.onLockPlayer2(socket);
+        events.onLockPlayer2(socket);
     });
 
     socket.on('init game', function() {
-      events.onInitGame();
+        events.onInitGame();
     });
 
     socket.on('disconnect', function() {
-      events.onDisconnect(socket);
+        events.onDisconnect(socket);
     });
 
-   socket.on('register tick', function(data) { 
-	events.onRegisterTick(socket,data);
-   });
+    socket.on('register tick', function(data) {
+        events.onRegisterTick(socket, data);
+    });
 
 
 });
 
-http.listen(3000, function () {
+http.listen(3000, function() {
     console.log("listening on *: 3000");
 });
 
@@ -56,15 +56,15 @@ http.listen(3000, function () {
 
 var events = {
 
- 
 
-    onConnect: function (socket) {
+
+    onConnect: function(socket) {
 
         clients[socket.client.id] = socket;
         console.log("User connected: " + socket.client.id);
 
         console.log(currentState);
-        switch(currentState) {
+        switch (currentState) {
             case 'waiting':
                 events.showWhenWaiting(socket);
                 break;
@@ -74,13 +74,13 @@ var events = {
             case 'game':
                 events.showInGame(socket);
                 break;
-	    case 'completed':
-		events.showCompleted(socket);
+            case 'completed':
+                events.showCompleted(socket);
         }
 
     },
 
-    onLockPlayer1: function (socket) {
+    onLockPlayer1: function(socket) {
         if (helper.checkIfClientIsPlayer(socket.client.id)) {
             return;
         }
@@ -90,14 +90,14 @@ var events = {
         socket.emit('change player 1 text');
         console.log("Player 1 Ready: " + socket.client.id);
 
-        if(helper.checkBothPlayersReady()) {
+        if (helper.checkBothPlayersReady()) {
             this.onGameReady(socket);
         }
 
 
     },
 
-    onLockPlayer2: function (socket) {
+    onLockPlayer2: function(socket) {
 
         if (helper.checkIfClientIsPlayer(socket.client.id)) {
             return;
@@ -108,16 +108,16 @@ var events = {
         socket.emit('change player 2 text');
         console.log("Player 2 Ready: " + socket.client.id);
 
-        if(helper.checkBothPlayersReady()) {
+        if (helper.checkBothPlayersReady()) {
             this.onGameReady(socket);
         }
 
     },
 
-    onDisconnect: function (socket) {
+    onDisconnect: function(socket) {
 
-        if(helper.checkIfClientIsPlayer()) {
-            if(player1 == socket) {
+        if (helper.checkIfClientIsPlayer()) {
+            if (player1 == socket) {
                 player1 = null;
                 io.emit('reset button player 1');
             } else {
@@ -129,137 +129,257 @@ var events = {
         delete clients[socket.client.id];
     },
 
-    onGameReady : function(socket) { 
-	currentState = 'ready';
-	io.emit('game ready');
+    onGameReady: function(socket) {
+        currentState = 'ready';
+        io.emit('game ready');
+        timer = 5;
+        var intervalId = setInterval(function() {
+            if (timer == 0) {
+                clearInterval(intervalId);
+                helper.createGame();
+                currentState = 'game';
+                io.emit('show game', game);
+                return;
+            }
 
-	 var intervalId = setInterval(function(){
-	    if (timer == 0) {
-	      clearInterval(intervalId);
-	      helper.createGame();
-	      currentState = 'game';
-      	      io.emit('show game', game);
-	      return;
-	    }
+            io.emit('change title', {
+                message: "GAME STARTING IN " + timer + "..."
+            });
 
-	    io.emit('change title', {message : "GAME STARTING IN " + timer + "..."});
-	   
-	    timer--;
-	  },1000);
-   },
-
-    onRegisterTick : function(socket, data) { 
-	if((game.turn == 1 && player1 == socket) || (game.turn == 2 && player2 == socket)) { 
-		if(game.board[data.x][data.y] != undefined) return;
-		game.board[data.x][data.y] = game.turn;
-		game.turn = (game.turn == 1) ? 2 : 1;
-		io.emit('apply tick', { x : data.x, y : data.y, turn : game.turn });
-		if(helper.checkPlayerWin()) { 
-			player1 = null;
-			player2 = null;
-			currentState = 'completed';
-			var playerWon = game.turn == 1 ? 2 : 1;
-			io.emit('show winning positions', { winningPositions : winningPositions, player : playerWon });
-			timer = 5;
-	
-			var intervalId = setInterval(function(){
-	    		if (timer == 0) {
-	      		clearInterval(intervalId);
-	      
-	      			return;
-	   		 }
-
-	    		io.emit('change title', {message : "PLAYER " + playerWon + " HAS WON! NEW GAME WILL BEGIN IN " + timer + "..." });
-	   
-		    timer--;
-		  },1000);
-			}
-	}
-		
+            timer--;
+        }, 1000);
     },
 
- 
+    onRegisterTick: function(socket, data) {
+        if ((game.turn == 1 && player1 == socket) || (game.turn == 2 && player2 == socket)) {
+            if (game.board[data.x][data.y] != undefined) return;
 
-    showWhenWaiting : function(socket) {
-      socket.emit('show when waiting',
-      { player1Button : player1 != null, player2Button : player2 != null});
+            io.emit('apply tick', {
+                x: data.x,
+                y: data.y,
+                turn: game.turn
+            });
+            game.turnCounter++;
+            game.board[data.x][data.y] = game.turn;
+            game.turn = (game.turn == 1) ? 2 : 1;
+            if (helper.checkPlayerWin()) {
+
+                player1 = null;
+                player2 = null;
+                currentState = 'completed';
+                var playerWon = game.turn == 1 ? 2 : 1;
+                io.emit('show winning positions', {
+                    winningPositions: winningPositions,
+                    player: playerWon
+                });
+                timer = 5;
+
+                var intervalId = setInterval(function() {
+                    if (timer == 0) {
+                        clearInterval(intervalId);
+                        io.emit('reset');
+                        currentState = 'waiting';
+                        return;
+                    }
+
+                    io.emit('change title', {
+                        message: "PLAYER " + playerWon + " HAS WON! NEW GAME WILL BEGIN IN " + timer + "..."
+                    });
+
+                    timer--;
+                }, 1000);
+            } else {
+                if (game.turnCounter == 9) {
+                    player1 = null;
+                    player2 = null;
+                    currentState = 'completed';
+                    timer = 5;
+                    var intervalId = setInterval(function() {
+                        if (timer == 0) {
+                            clearInterval(intervalId);
+                            io.emit('reset');
+                            currentState = 'waiting';
+                            return;
+                        }
+
+                        io.emit('change title', {
+                            message: "DRAW! NEW GAME WILL BEGIN IN " + timer + "..."
+                        });
+
+                        timer--;
+                    }, 1000);
+                } else {
+                    io.emit('change title', {
+                        message: "PLAYER " + game.turn + "'S TURN"
+                    });
+                }
+
+            }
+
+
+        }
 
     },
 
-    showWhenReady : function(socket) {
-      socket.emit('game ready');
-      socket.emit('display counter', timer);
+
+
+    showWhenWaiting: function(socket) {
+        socket.emit('show when waiting', {
+            player1Button: player1 != null,
+            player2Button: player2 != null
+        });
+
     },
 
-    showInGame : function(socket) {
-      socket.emit('show game', game);
+    showWhenReady: function(socket) {
+        socket.emit('game ready');
+        socket.emit('display counter', timer);
     },
 
-    showCompleted : function(socket) {
-	var playerWon = game.turn == 1 ? 2 : 1;
-	socket.emit('show game', game);
-	socket.emit('show winning positions', { winningPositions : winningPositions, player : playerWon });
-	socket.emit('change title', {message : "PLAYER " + playerWon + " HAS WON! NEW GAME WILL BEGIN IN " + timer + "..." });
+    showInGame: function(socket) {
+        socket.emit('show game', game);
+    },
+
+    showCompleted: function(socket) {
+        var playerWon = game.turn == 1 ? 2 : 1;
+        socket.emit('show game', game);
+        socket.emit('show winning positions', {
+            winningPositions: winningPositions,
+            player: playerWon
+        });
+        socket.emit('change title', {
+            message: "PLAYER " + playerWon + " HAS WON! NEW GAME WILL BEGIN IN " + timer + "..."
+        });
     }
-
-
-
-
 };
 
 var helper = {
 
-    checkIfClientIsPlayer : function (id) {
-       return player1 == clients[id] || player2 == clients[id] ? true : false;
+    checkIfClientIsPlayer: function(id) {
+        return player1 == clients[id] || player2 == clients[id] ? true : false;
     },
-    checkBothPlayersReady : function() {
+    checkBothPlayersReady: function() {
         return player1 != null && player2 != null;
     },
 
-    createGame : function() { 
-	game = {
+    createGame: function() {
+        game = {
 
-          board: [
-            [,,],
-            [,,],
-            [,,]
-          ],
+            board: [
+                [, , ],
+                [, , ],
+                [, , ]
+            ],
 
-          turn : Math.floor((Math.random() * 2) + 1)
+            turn: Math.floor((Math.random() * 2) + 1),
 
-      };
-    }, 
+            turnCounter: 0
 
-   checkPlayerWin : function() {
+        };
+    },
 
-		if( game.board[0][0] === game.board[0][1] && game.board[0][2] === game.board[0][0] && game.board[0][1] === game.board[0][2] && game.board[0][0] != undefined)  { 
-			winningPositions = [{x : 0, y : 0}, {x : 0, y : 1}, {x : 0, y : 2}];
-			return true;
-		} else if(game.board[1][0] === game.board[1][1] && game.board[1][0] === game.board[1][2] && game.board[1][2] === game.board[1][1] && game.board[1][0] != undefined) { 
-			winningPositions = [{x : 1, y : 0}, {x : 1, y : 1}, {x : 1, y : 2}];
-			return true;
-		} else if(game.board[2][0] === game.board[2][1] && game.board[2][0] === game.board[2][2] && game.board[2][2] === [2][1] && game.board[2][0] != undefined) { 
-			winningPositions = [{x : 2, y : 0}, {x : 2, y : 1}, {x : 2, y : 2}];
-			return true;
-		} else if(game.board[0][0] === game.board[1][0] && game.board[0][0] === game.board[2][0] && game.board[2][0] === game.board[1][0] && game.board[0][0] != undefined) { 
-			winningPositions = [{x : 0, y : 0}, {x : 1, y : 0}, {x : 2, y : 0}];
-			return true;
-		} else if(game.board[0][1] == game.board[1][1] && game.board[0][1] == game.board[2][1] && game.board[2][1] == game.board[1][1] && game.board[0][1] != undefined) { 
-			winningPositions = [{x : 0, y : 1}, {x : 1, y : 1}, {x : 2, y : 1}];
-			return true;
-		} else if(game.board[0][2] == game.board[1][2] && game.board[0][2] == game.board[2][2] && game.board[2][2] == game.board[1][2] && game.board[0][2] != undefined) { 
-			winningPositions = [{x : 0, y : 2}, {x : 1, y : 2}, {x : 2, y : 2}];
-			return true;
-		} else if(game.board[0][0] == game.board[1][1] && game.board[0][0] == game.board[2][2] && game.board[2][2] == game.board[1][1] && game.board[0][0] != undefined) { 
-			winningPositions = [{x : 0, y : 0}, {x : 1, y : 1}, {x : 2, y : 2}];
-			return true;
-		} else if(game.board[0][2] == game.board[1][1] && game.board[0][2] == game.board[2][0] && game.board[2][0] == game.board[1][1] && game.board[0][2] != undefined) { 
-			winningPositions = [{x : 0, y : 2}, {x : 1, y : 1}, {x : 2, y : 0}];
-			return true;
-		} else { 
-			return false;
-		}
+    checkPlayerWin: function() {
 
-		
-	}
+        if (game.board[0][0] === game.board[0][1] && game.board[0][2] === game.board[0][0] && game.board[0][1] === game.board[0][2] && game.board[0][0] != undefined) {
+            winningPositions = [{
+                x: 0,
+                y: 0
+            }, {
+                x: 0,
+                y: 1
+            }, {
+                x: 0,
+                y: 2
+            }];
+            return true;
+        } else if (game.board[1][0] === game.board[1][1] && game.board[1][0] === game.board[1][2] && game.board[1][2] === game.board[1][1] && game.board[1][0] != undefined) {
+            winningPositions = [{
+                x: 1,
+                y: 0
+            }, {
+                x: 1,
+                y: 1
+            }, {
+                x: 1,
+                y: 2
+            }];
+            return true;
+        } else if (game.board[2][0] === game.board[2][1] && game.board[2][0] === game.board[2][2] && game.board[2][2] === game.board[2][1] && game.board[2][0] != undefined) {
+            winningPositions = [{
+                x: 2,
+                y: 0
+            }, {
+                x: 2,
+                y: 1
+            }, {
+                x: 2,
+                y: 2
+            }];
+            return true;
+        } else if (game.board[0][0] === game.board[1][0] && game.board[0][0] === game.board[2][0] && game.board[2][0] === game.board[1][0] && game.board[0][0] != undefined) {
+            winningPositions = [{
+                x: 0,
+                y: 0
+            }, {
+                x: 1,
+                y: 0
+            }, {
+                x: 2,
+                y: 0
+            }];
+            return true;
+        } else if (game.board[0][1] == game.board[1][1] && game.board[0][1] == game.board[2][1] && game.board[2][1] == game.board[1][1] && game.board[0][1] != undefined) {
+            winningPositions = [{
+                x: 0,
+                y: 1
+            }, {
+                x: 1,
+                y: 1
+            }, {
+                x: 2,
+                y: 1
+            }];
+            return true;
+        } else if (game.board[0][2] == game.board[1][2] && game.board[0][2] == game.board[2][2] && game.board[2][2] == game.board[1][2] && game.board[0][2] != undefined) {
+            winningPositions = [{
+                x: 0,
+                y: 2
+            }, {
+                x: 1,
+                y: 2
+            }, {
+                x: 2,
+                y: 2
+            }];
+            return true;
+        } else if (game.board[0][0] == game.board[1][1] && game.board[0][0] == game.board[2][2] && game.board[2][2] == game.board[1][1] && game.board[0][0] != undefined) {
+            winningPositions = [{
+                x: 0,
+                y: 0
+            }, {
+                x: 1,
+                y: 1
+            }, {
+                x: 2,
+                y: 2
+            }];
+            return true;
+        } else if (game.board[0][2] == game.board[1][1] && game.board[0][2] == game.board[2][0] && game.board[2][0] == game.board[1][1] && game.board[0][2] != undefined) {
+            winningPositions = [{
+                x: 0,
+                y: 2
+            }, {
+                x: 1,
+                y: 1
+            }, {
+                x: 2,
+                y: 0
+            }];
+            return true;
+        } else {
+            return false;
+        }
+
+
+    }
 };
